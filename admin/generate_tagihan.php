fb060c891674d28e9f61e006369e9010ca13a703
aureliasept <aureliaseptiani51@gmail.com>
2025-07-14 11:45:27 +0700

    Otomatisasi Pembuatan Tagihan Bulanan
     Membuat skrip untuk menghasilkan tagihan bulanan secara otomatis berdasarkan data penghuni dan kamar.

---
+++ admin/generate_tagihan.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+<h4>Generate Tagihan Bulanan</h4>
+<p>Fitur ini akan membuat tagihan untuk semua penghuni aktif di awal bulan.</p>
+<form method="post">
+    <button type="submit" class="btn btn-primary">Generate Tagihan Bulan Ini</button>
+</form>
+<!-- Notifikasi hasil proses akan ditampilkan di sini -->
+<?php include 'footer.php'; ?> 
